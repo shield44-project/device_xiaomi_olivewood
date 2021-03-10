7d7d64f98cf8eff1cee73f3716b2f848a828a108
Tobias Merkel <Stylogey@gmail.com>
2021-03-10 17:31:16 +0000

    olivewood: Add `DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX`
    Change-Id: I5c0003bce62307b4467ecd9adbfee2c3b26064a4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,9 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/device_framework_compatibility_matrix.xml \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
