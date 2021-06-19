5483c907ec950d5465f6170f925bf52f77602248
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-06-19 21:42:15 +0200

    olivewood: Inherit product_launched_with_p makefile
    Change-Id: Iadfa6184ac332b48b6ed1aad767ca0ffc3ef8334
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 $(call inherit-product, vendor/xiaomi/olivewood/olivewood-vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 TARGET_BOARD_PLATFORM := msm8937
 TARGET_BOARD_SUFFIX := _64
