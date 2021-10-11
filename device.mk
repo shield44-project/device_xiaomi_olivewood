4867772b0db79b934654548f2ea931a2c923ab96
shield44 <motoecondorxt1022@gmail.com>
2021-10-11 19:59:35 +0530

    olivewood: Move keylayouts to configs
    Change-Id: Ic578b14fe3e1f9f0c08149c4fbaaad4c58f1d76e
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -225,7 +225,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
 # Keymaster
 PRODUCT_PACKAGES += \
