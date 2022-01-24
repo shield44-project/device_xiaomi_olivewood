122655f53390971311b286b0a586681ca11c23dc
xboxfanj <jake@aospa.co>
2022-01-24 15:03:16 +0530

    olivewood: Makefile: Drop powerhal
    • As per
    [1] - https://gerrit.aospa.co/c/AOSPA/android_vendor_qcom-opensource_power/+/9795
    [2] - https://gerrit.aospa.co/c/AOSPA/android_device_pa_sepolicy/+/11473
    
    Change-Id: Ie78515838618c2c9eeb72033bc89edb86e533ef9
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -288,7 +288,6 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor \
     vendor.qti.hardware.perf@2.1.vendor
 
