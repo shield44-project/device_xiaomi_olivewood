f2fa91cd8560a3c83e926e68ba01268a6147f41e
shield44 <shield44@pixelexperience.org>
2022-02-06 12:17:57 +0000

    olivewood: drop parts leftover
    Signed-off-by: shield44 <shield44@pixelexperience.org>
    Change-Id: Ibd11710f9a50af650b072f92a7c8adf18ad5f6c4

--- device.mk
+++ device.mk
@@ -294,7 +294,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.recovery.qcom.usb.rc \
-    init.parts.rc \
     init.target.rc \
     ueventd.qcom.rc \
     init.insmod.sh
