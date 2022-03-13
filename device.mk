3a85e3eab82743ed9a7ad262f719b8b69914b2ff
Dylan Neve <dylanneve1@gmail.com>
2022-03-13 21:20:36 +0000

    olivewood: audio: Uprev SoundTrigger HAL to 2.3
    * Fixes intermittent issues with Google Assistant voice activation.
    
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.primary.msm8937:32 \
     audio.r_submix.default \
